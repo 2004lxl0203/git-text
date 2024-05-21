7aa73c8d437ea816fcc51d403787e4193425a4e3
LXL <2129249080@qq.com>
2024-05-21 18:00:06 +0800

    commit2

--- day1/src/com/itheima/demo/text.java
+++ day1/src/com/itheima/demo/text.java
@@ -3,5 +3,6 @@ package com.itheima.demo;
 public class text {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
